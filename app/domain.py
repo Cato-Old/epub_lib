752d97991c53c8d858fa66f7add593c58a53e8e1
Wojciech Oczkowski <wojciech.oczkowski@yougov.com>
2019-12-02 14:31:14 +0100

    add Paragraph object basic initialization

--- app/domain.py
+++ app/domain.py
@@ -1,2 +1,3 @@
 class Paragraph:
-    pass
+    def __init__(self, input: str):
+        self.content = input
--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
 
+from factory import Faker
+
 from app.domain import Paragraph
 
 
 class ParagraphTest(TestCase):
-    def test_can_import_class(self):
-        Paragraph
+    def setUp(self) -> None:
+        self.fake_sentence = Faker('sentence')
+
+    def test_can_initialize_with_passing_one_line_string(self) -> None:
+        payload = self.fake_sentence.generate()
+        paragraph = Paragraph(payload)
+        self.assertEqual(payload, paragraph.content)
