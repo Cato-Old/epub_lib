17d0adb57517117d3375e6d3b8eca610518a6af7
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-12-03 21:11:14 +0100

    add Paragraph object basic input validation

--- app/domain.py
+++ app/domain.py
@@ -1,3 +1,5 @@
 class Paragraph:
-    def __init__(self, input: str):
-        self.content = input
+    def __init__(self, input_str: str):
+        if '\n' in input_str:
+            raise ValueError
+        self.content = input_str
--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,3 +1,4 @@
+from importlib import import_module
 from unittest import TestCase
 
 from factory import Faker
@@ -5,11 +6,29 @@ from factory import Faker
 from app.domain import Paragraph
 
 
+def get_provider(locale: str = None):
+    locale = locale if locale else 'en_US'
+    base_provider = import_module(f'faker.providers.lorem.{locale}')
+
+    class Provider(base_provider.Provider):
+        def text_with_newline(self):
+            return f'{self.paragraph()}\n{self.paragraph()}'
+
+    return Provider
+
+
 class ParagraphTest(TestCase):
     def setUp(self) -> None:
-        self.fake_sentence = Faker('sentence')
+        faker = Faker
+        faker.add_provider(get_provider())
+        self.fake_sentence = faker('sentence')
+        self.fake_text = faker('text_with_newline')
 
-    def test_can_initialize_with_passing_one_line_string(self) -> None:
+    def test_can_initialize_with_passing_oneline_string(self) -> None:
         payload = self.fake_sentence.generate()
         paragraph = Paragraph(payload)
         self.assertEqual(payload, paragraph.content)
+
+    def test_raises_when_initialized_with_multiline_string(self):
+        with self.assertRaises(ValueError):
+            Paragraph(self.fake_text.generate())
