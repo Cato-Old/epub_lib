1ae72fd86c4fa20795af0b4a4939e6a746c73e2f
Wojciech Oczkowski <wojciech.oczkowski@yougov.com>
2019-12-18 14:45:01 +0100

    add settings initialization in main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
+import os
 import sys
 
 from app.domain import Book
+from app.settings import Settings
 
 
 def main() -> None:
     input_path = sys.argv[1]
+    Settings(base_path=os.path.dirname(__file__))
     Book(input_path).dump()
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from unittest.mock import patch
 
 from app.main import main
+from app.settings import Settings
 
 
 @patch('app.main.sys.argv', ['name', 'path_name'])
@@ -14,3 +15,8 @@ class MainTest(TestCase):
     def test_calls_proper_method_on_book(self, mock_book) -> None:
         main()
         mock_book.return_value.dump.assert_called_once()
+
+    @patch('app.main.os.path.abspath', return_value='foo/bar/baz')
+    def test_initiates_settings_singleton(self, mock_book, mock_os) -> None:
+        main()
+        self.assertEqual(Settings().base_path, 'foo/bar/baz')
