b9d96ff0441f81b512f30304e3d117edb21d1be3
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-12-22 08:21:52 +0100

    add basic italic markers processing

--- app/processing.py
+++ app/processing.py
@@ -1,10 +1,23 @@
+import re
+
 from app.domain.paragraph_type import ParagraphType
 
 
 class ParagraphProcessor:
     @staticmethod
     def process(raw_text: str, paragraph_type: ParagraphType) -> str:
+        text = ParagraphProcessor.process_type_marker(raw_text, paragraph_type)
+        return ParagraphProcessor.process_italic_marker(text)
+
+    @staticmethod
+    def process_type_marker(
+            raw_text: str, paragraph_type: ParagraphType,
+    ) -> str:
         if paragraph_type == ParagraphType.CONTINUATION:
             return raw_text
         else:
             return raw_text[paragraph_type.ind:]
+
+    @staticmethod
+    def process_italic_marker(raw_text: str) -> str:
+        return re.sub(r'€([^€<]+)€', lambda m: f'<i>{m.group(1)}</i>', raw_text)
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,9 @@
+from random import randint
 from unittest import TestCase
 
+from factory import Faker
+
+from app.domain import ParagraphType
 from app.processing import ParagraphProcessor
 from tests.test_domain import PARAGRAPH_TEST_PARAMS
 
@@ -14,3 +18,15 @@ class ParagraphProcessorTest(TestCase):
             with self.subTest(tested_type):
                 actual = self.processor.process(content, tested_type)
                 self.assertRegex(actual, regex_content)
+
+    def test_processes_raw_text_italic_markers(self) -> None:
+        payload = self.sentence_with_italic()
+        actual = self.processor.process(payload, ParagraphType.CONTINUATION)
+        self.assertRegex(actual, r'.*<i>.+</i>.*')
+
+    def sentence_with_italic(self) -> str:
+        words = Faker('words', nb=randint(1, 10)).generate()
+        ind = randint(0, len(words)-1)
+        words[ind] = f'€{words[ind]}€'
+        words[0] = words[0].title()
+        return ' '.join(words) + '.'
