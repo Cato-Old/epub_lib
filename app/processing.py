f366f876fda2d4aec83a23acdd78dcdb9484bfde
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2020-01-09 20:29:55 +0100

    add widow processing

--- app/processing.py
+++ app/processing.py
@@ -14,7 +14,8 @@ class ParagraphProcessor:
     @staticmethod
     def process(paragraph: ProcessedParagraph) -> ProcessedParagraph:
         paragraph = ParagraphProcessor.process_type_marker(paragraph)
-        return ParagraphProcessor.process_italic_marker(paragraph)
+        paragraph = ParagraphProcessor.process_italic_marker(paragraph)
+        return ParagraphProcessor.process_widow(paragraph)
 
     @staticmethod
     def process_type_marker(
@@ -37,4 +38,16 @@ class ParagraphProcessor:
             lambda m: f'<i>{m.group(1)}</i>',
             paragraph.text,
         )
+        return ProcessedParagraph(text=text, type=paragraph.type)
+
+    @staticmethod
+    def process_widow(paragraph: ProcessedParagraph) -> ProcessedParagraph:
+        text = paragraph.text
+        for widow in 'wiouza':
+            text = re.sub(
+                f'([ ;]){widow} ',
+                lambda m: f'{m.group(1)}{widow}&nbsp;',
+                text,
+            )
+            text = re.sub(f'{widow.upper()} ', f'{widow.upper()}&nbsp;', text)
         return ProcessedParagraph(text=text, type=paragraph.type)
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, shuffle
 from unittest import TestCase
 
 from factory import Faker
@@ -27,9 +27,27 @@ class ParagraphProcessorTest(TestCase):
         actual = self.processor.process(paragraph)
         self.assertRegex(actual.text, r'.*<i>.+</i>.*')
 
+    def test_processes_raw_text_widows(self) -> None:
+        paragraph = ProcessedParagraph(
+            text=self.sentence_with_widows(), type=ParagraphType.CONTINUATION,
+        )
+        actual = self.processor.process(paragraph)
+        for widow in 'wiouza':
+            with self.subTest(widow):
+                self.assertNotRegex(actual.text, f' {widow} ')
+                self.assertNotRegex(actual.text, f'{widow.upper()} ')
+
     def sentence_with_italic(self) -> str:
         words = Faker('words', nb=randint(1, 10)).generate()
         ind = randint(0, len(words)-1)
         words[ind] = f'€{words[ind]}€'
+        words[0] = words[0].title()
+        return ' '.join(words) + '.'
+
+    def sentence_with_widows(self) -> str:
+        words = Faker('words', nb=randint(10, 20)).generate()
+        words.extend('wiouza')
+        while words[-1] in 'wiouza':
+            shuffle(words)
         words[0] = words[0].title()
         return ' '.join(words) + '.'
